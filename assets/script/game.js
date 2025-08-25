0cb75b65cfe9b176525abeff53dadf938a547074
nataliafrancisca <nathifrancisca@gmail.com>
2025-08-24 23:58:07 -0300

    feat: adicionando animação na troca dos jogadores.

--- assets/script/game.js
+++ assets/script/game.js
@@ -72,6 +72,71 @@ function clean(){
     }
 }
 
+function runAnimationWhile(athleteOne, athleteTwo){
+    const rect1 = athleteOne.getBoundingClientRect();
+    const rect2 = athleteTwo.getBoundingClientRect();
+ 
+    const dx = rect2.left - rect1.left;
+    const dy = rect2.top - rect1.top;
+
+    athleteOne.style.transition = "transform 0.6s ease";
+    athleteTwo.style.transition = "transform 0.6s ease";
+
+    athleteOne.style.transform = `translate(${dx}px, ${dy}px)`;
+    athleteTwo.style.transform = `translate(${-dx}px, ${-dy}px)`;
+
+    setTimeout(() => {
+        athleteOne.style.transform = "";
+        athleteTwo.style.transform = "";
+    }, 1000);
+
+    setTimeout(() => {
+        athleteTwo.classList.add()
+    }, 1000);
+}
+
+function runAnimationFrontRow(){
+    const frontRow = document.querySelectorAll(".line__1.inserted");
+
+    switch(currentRotation){
+        case 1: 
+            runAnimationWhile(frontRow[0], frontRow[2])
+            break;
+        case 2:
+            runAnimationWhile(frontRow[1], frontRow[2])
+            break;
+        case 3:
+            runAnimationWhile(frontRow[0], frontRow[1])
+            break;
+        case 4:
+            runAnimationWhile(frontRow[0], frontRow[2])
+            break;
+        case 5:
+            runAnimationWhile(frontRow[1], frontRow[2])
+            break;
+        case 6:
+            runAnimationWhile(frontRow[0], frontRow[1])
+            break;
+        default:
+            console.log("no effect");
+    }
+}
+
+function runAnimationBackRow(){
+    const backRow = document.querySelectorAll(".line__4.inserted, .line__5.inserted");
+
+    switch(currentRotation){
+        case 1: 
+            runAnimationWhile(backRow[0], backRow[2])
+            break;
+        case 4:
+            runAnimationWhile(backRow[0], backRow[2])
+            break;
+        default:
+            console.log("no effect");
+    }
+}
+
 function showFirstRotation(){
     clean();
 
@@ -82,6 +147,9 @@ function showFirstRotation(){
     insertIntoBackRow(players, rotation.back, true);
 
     currentSetterPosition.lastChild.innerHTML = baseGameRotation[currentRotation];
+
+    runAnimationFrontRow();
+    runAnimationBackRow();
 }
 
 function next(){
@@ -90,6 +158,9 @@ function next(){
     currentSetterPosition.lastChild.innerHTML = baseGameRotation[currentRotation];
 
     serving();
+
+    runAnimationFrontRow();
+    runAnimationBackRow();
 }
 
 function back(){
@@ -98,6 +169,8 @@ function back(){
     currentSetterPosition.lastChild.innerHTML = baseGameRotation[currentRotation];
 
     serving();
+    runAnimationFrontRow();
+    runAnimationBackRow();
 }
 
 function serving(){
@@ -127,4 +200,3 @@ btnActionNextRotation.addEventListener("click", () => next());
 btnActionBackRotation.addEventListener("click", () => back());
 
 showFirstRotation();
-updateSchemaAfterFormSubmit();

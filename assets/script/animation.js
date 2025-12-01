b4f9628b55983f56b824f403d0f97fa688bbe804
nataliafrancisca <nathifrancisca@gmail.com>
2025-11-30 23:15:05 -0300

    feat: adicionando os jogadores dentro da quadra

--- assets/script/animation.js
+++ assets/script/animation.js
@@ -1,72 +1,60 @@
-import { rotations } from '../db/rotation.js';
-
-const currentGameAction = document.getElementById("p__current-action");
-
-function runAnimationWhile(goal, origin){
-    const rect1 = goal.getBoundingClientRect();
-    const rect2 = origin.getBoundingClientRect();
-
+function runAnimationWhile(athleteOne, athleteTwo){
+    const rect1 = athleteOne.getBoundingClientRect();
+    const rect2 = athleteTwo.getBoundingClientRect();
+ 
     const dx = rect2.left - rect1.left;
     const dy = rect2.top - rect1.top;
 
-    goal.style.transition = "transform 0.6s ease";
-    origin.style.transition = "transform 0.6s ease";
+    athleteOne.style.transition = "transform 0.6s ease";
+    athleteTwo.style.transition = "transform 0.6s ease";
 
-    goal.style.transform = `translate(${dx}px, ${dy}px)`;
-    origin.style.transform = `translate(${-dx}px, ${-dy}px)`;
-
-    setTimeout(() => {
-        goal.style.transform = "";
-        origin.style.transform = "";
-    }, 2000);
+    athleteOne.style.transform = `translate(${dx}px, ${dy}px)`;
+    athleteTwo.style.transform = `translate(${-dx}px, ${-dy}px)`;
 
     setTimeout(() => {
-        origin.classList.add()
-        goal.classList.remove("set__temporarie-bg-blue");
-        goal.classList.remove("set__temporarie-bg-orange");
-
-        origin.classList.remove("set__temporarie-bg-blue");
-        origin.classList.remove("set__temporarie-bg-orange");
-    }, 2000);
+        athleteOne.style.transform = "";
+        athleteTwo.style.transform = "";
+    }, 1000);
 }
 
-function setTemporarieBackgroundColor(origin, goal){
-    const bgOrigin = window.getComputedStyle(origin).backgroundColor;
-    const bgGoal = window.getComputedStyle(goal).backgroundColor;
+export function runAnimationFrontRow(currentRotation){
+    const frontRow = document.querySelectorAll(".line__1.inserted");
 
-    if(bgOrigin == "rgb(255, 165, 0)" && bgGoal == "rgb(61, 106, 255)"){
-        goal.classList.add("set__temporarie-bg-orange");
-    }
-
-    if(bgOrigin == "rgb(34, 255, 34)" && bgGoal == "rgb(255, 165, 0)"){
-        goal.classList.add("set__temporarie-bg-blue");
-    }
-
-    if(bgOrigin == "rgb(61, 106, 255)" && bgGoal == "rgb(255, 165, 0)"){
-        goal.classList.add("set__temporarie-bg-blue");
-        origin.classList.add("set__temporarie-bg-orange");
-    }
-
-    runAnimationWhile(goal, origin)
-}
-
-export function runAnimation(currentRotation){
-    const isAnAttackAction = currentGameAction.innerText.includes("realizando ataque");
-    
     switch(currentRotation){
-
-        case 1 & isAnAttackAction:
-
-            for (let i = 1; i < 7; i++){
-                const attack = rotations[currentRotation]["attack"][i];
-                const attackgoal = rotations[currentRotation]["attack-goal"][i];
-
-                const origin = document.getElementById(`${attack[0]}-${attack[1]}`)
-                const goal = document.getElementById(`${attackgoal[0]}-${attackgoal[1]}`)
-
-                setTemporarieBackgroundColor(origin, goal);
-            }
-           
+        case 1: 
+            runAnimationWhile(frontRow[0], frontRow[2])
             break;
+        case 2:
+            runAnimationWhile(frontRow[0], frontRow[1])
+            break;
+        case 3:
+            runAnimationWhile(frontRow[0], frontRow[1])
+            break;
+        case 4:
+            runAnimationWhile(frontRow[0], frontRow[2])
+            break;
+        case 5:
+            runAnimationWhile(frontRow[1], frontRow[2])
+            break;
+        case 6:
+            runAnimationWhile(frontRow[0], frontRow[1])
+            break;
+        default:
+            console.log("no effect");
+    }
+}
+
+export function runAnimationBackRow(currentRotation){
+    const backRow = document.querySelectorAll(".line__4.inserted, .line__5.inserted");
+
+    switch(currentRotation){
+        case 1: 
+            runAnimationWhile(backRow[0], backRow[2])
+            break;
+        case 4:
+            runAnimationWhile(backRow[0], backRow[2])
+            break;
+        default:
+            console.log("no effect");
     }
 }
